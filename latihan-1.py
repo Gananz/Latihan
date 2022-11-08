d86349230abfd1989b6dc68012ad17c1a4c6d458
unknown <sronohanan@gmail.com>
2022-11-08 08:07:13 +0700

    hanan

---
+++ latihan-1.py
@@ -0,0 +1,19 @@
+#soal-1#
+print ("hello world python")
+
+#soal-2#
+print (69 + 69)
+print (69 - 69)
+print (69*69)
+print (69/69)
+
+#soal-3#
+var1 = 138
+var2 = 2
+print (var1 / var2)
+print (var1 // var2)
+
+#soal-4#
+usia = "14 tahun"
+kata = "usiaku"
+print ("{} {}". format (kata,usia))
---
+++ nyoba.py
@@ -0,0 +1,7 @@
+nama = input("nama anda :")
+print (nama)
+
+
+x = input("yang mau di ulang: ")
+y = int(input("ulang berapa kali: "))
+print(x*y)
